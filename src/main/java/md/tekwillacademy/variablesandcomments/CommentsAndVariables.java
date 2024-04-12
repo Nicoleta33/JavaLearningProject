00f10e152ff5303e581a31b2a5e92bd950e3b317
Nicoleta33 <nicoleta1curmei@gmail.com>
2024-04-12 13:28:42 -0500

    Declared all variables and their initialization.

---
+++ src/main/java/md/tekwillacademy/variablesandcomments/CommentsAndVariables.java
@@ -0,0 +1,51 @@
+package md.tekwillacademy.variablesandcomments;
+
+public class CommentsAndVariables {
+
+    public static void main(String[] args) {
+
+     //Declare our variable
+
+        boolean falseBooleanVariable;
+        boolean trueBooleanVariable;
+        char firstLetterFromAscii;
+        char lastLetterFromAscii;
+        byte byteLowerRangeDataType;
+        byte byteHigherRangeDataType;
+        short shortLowerRangeDataType;
+        short shortHigherRangeDataType;
+        int intLowerRangeDataType;
+        int intHigherRangeDataType;
+        long longLowerRangeDataType;
+        long longHigherRangeDataType;
+        float floatLowerRangeDataType;
+        float floatHigherRangeDataType;
+        double doubleLowerRangeDataType;
+        double doubleHigherDataType;
+
+        // We gonna initialize our variables that we just declare up
+        falseBooleanVariable = false;
+        trueBooleanVariable = true;
+        firstLetterFromAscii = 'A';
+        lastLetterFromAscii = 'Z';
+        byteLowerRangeDataType = -128;
+        byteHigherRangeDataType = 127;
+        shortLowerRangeDataType = -32768;
+        shortHigherRangeDataType = 32767;
+        intLowerRangeDataType = -2147483648;
+        intHigherRangeDataType = 2147483647;
+        longLowerRangeDataType = -9223372036854775808L;
+        longHigherRangeDataType = 9223372036854775807L;
+        floatLowerRangeDataType = -3.4E+38f;
+        floatHigherRangeDataType = 3.4E+38f;
+        doubleLowerRangeDataType = -1.7E+308d;
+        doubleHigherDataType = 1.7E+305;
+
+
+
+
+
+
+
+    }
+}
