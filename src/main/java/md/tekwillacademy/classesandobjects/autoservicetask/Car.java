40548f5c85fd814fe1b32e9be26aa90cb4074970
Nicoleta33 <nicoleta1curmei@gmail.com>
2024-04-19 09:24:17 -0500

    Added the Java Code for an Auto Service app

---
+++ src/main/java/md/tekwillacademy/classesandobjects/autoservicetask/Car.java
@@ -0,0 +1,26 @@
+package md.tekwillacademy.classesandobjects.autoservicetask;
+
+public class Car {
+    String make;
+    String colorofTheCar;
+    int numberOfDoors;
+    int motorCapacity;
+
+public Car(String make, String colorOfTheCar, int numberOfDoors, int motoCapacity){
+    System.out.println("This is a full constructor with all the params included");
+    this.make=make;
+    this.colorofTheCar=colorOfTheCar;
+    this.numberOfDoors = numberOfDoors;
+    this.motorCapacity=motoCapacity;
+
+    System.out.println("The Object is created, bingo");
+
+
+
+
+
+}
+
+
+
+}
---
+++ src/main/java/md/tekwillacademy/classesandobjects/autoservicetask/Worker.java
@@ -0,0 +1,20 @@
+package md.tekwillacademy.classesandobjects.autoservicetask;
+
+import java.sql.SQLOutput;
+
+public class Worker {
+    String name;
+    int yearsOfExperience;
+    int pricePerHour;
+
+    public Worker (String name){
+        this.name = name;
+
+    }
+
+    public Worker(){
+        System.out.println("Welcome, good job, no one know what you just created");
+
+
+    }
+}
