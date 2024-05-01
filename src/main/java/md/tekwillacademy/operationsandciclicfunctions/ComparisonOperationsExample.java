f7cd76e624d12f200bfd22ff083642ddd942c58e
Nicoleta33 <nicoleta1curmei@gmail.com>
2024-05-01 11:07:04 -0500

    Added Operations And Defined Functions

---
+++ src/main/java/md/tekwillacademy/operationsandciclicfunctions/ComparisonOperationsExample.java
@@ -0,0 +1,40 @@
+package md.tekwillacademy.operationsandciclicfunctions;
+
+public class ComparisonOperationsExample {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        // Is equal
+        boolean isEqual = a == b;
+        System.out.println("Is equal: " + isEqual );
+
+        // Is not equal
+        boolean isNotEqual = a != b;
+        System.out.println("Is not equal: " + isNotEqual);
+
+        // Is greater than
+        boolean isGreaterThan = a > b;
+        System.out.println("Is greater than: " + isGreaterThan);
+
+        // Is less than
+        boolean isLessThan = a < b;
+        System.out.println("Is less than: " + isLessThan);
+
+        // Is greater than  or Equal
+        boolean isGreaterThanOrEqual = a >= b;
+        System.out.println("Is greater than or equal: " + isGreaterThanOrEqual);
+
+        // Is less than or equal
+        boolean isLessThanOrEqual = a <= b;
+        System.out.println("Is less than or equal: " + isLessThanOrEqual);
+
+
+
+
+
+
+
+    }
+}
