cd759afd8b187438e94f971fdd01e83c229db55d
Nicoleta33 <nicoleta1curmei@gmail.com>
2024-05-03 01:32:17 -0500

    Added the implementation of Zoo Service by using OOP

---
+++ src/main/java/md/tekwillacademy/zooclubservice/AnimalInterface.java
@@ -0,0 +1,9 @@
+package md.tekwillacademy.zooclubservice;
+
+public interface AnimalInterface {
+
+    // Toate clasele care implementeaza AnimalInterface va trebui sa faca override la makeSound() ,
+    // adica sa implementeze  metoda
+
+    void makeSound();
+}
---
+++ src/main/java/md/tekwillacademy/zooclubservice/Cat.java
@@ -0,0 +1,23 @@
+package md.tekwillacademy.zooclubservice;
+
+public class Cat extends Animal {
+
+
+    public Cat(String name) {
+        super(name);
+    }
+    @Override
+    public void makeSound() {
+
+    System.out.println(this.getName() + ": The cat is saying like: Meow, Meow, Meow");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println(this.getName() + ": The cat is eating fish food");
+    }
+}
+
+
+
+
